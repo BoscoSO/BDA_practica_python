2ec05d2f72d80be7e2b78a08cd522d2b7ceeca18
Josph4 <j.plopez1@udc.es>
2023-06-22 14:15:51 +0200

    Añadida la tabla descuentos y las especificaciones de las funcionalidades

--- version_2/bd/script_tablas.sql
+++ version_2/bd/script_tablas.sql
@@ -5,53 +5,79 @@ CREATE DATABASE pythonbda OWNER = bda;
 
 \c pythonbda bda;
 
-
-DROP TABLE Categoria;
-DROP TABLE Cliente;
+DROP TABLE Usuario;
 DROP TABLE Pelicula;
+DROP TABLE Categoria;
 DROP TABLE Alquiler;
+DROP TABLE Descuento;
 
 
-CREATE TABLE Categoria (
-    tipo        VARCHAR(16)   CONSTRAINT CategoriaPK PRIMARY KEY,
-    descripcion VARCHAR(1024) NOT NULL
+
+CREATE TABLE Usuario (
+    id          BIGSERIAL       CONSTRAINT UsuarioPK PRIMARY KEY,
+    nombre      VARCHAR(64)     NOT NULL,
+    apellidos   VARCHAR(64)     NOT NULL,
+    email       VARCHAR(128)    UNIQUE NOT NULL,
+    contrasena  VARCHAR(128)    NOT NULL,
+    telefono    VARCHAR(16)     UNIQUE NOT NULL,
+    fecha_alta  DATE            NOT NULL,
+    CONSTRAINT LengthContrasena CHECK (LENGTH(contrasena) >= 5)
 );
-
-
-CREATE TABLE Cliente (
-    email       VARCHAR(128) CONSTRAINT ClientePK PRIMARY KEY,
-    dni         VARCHAR(9)   UNIQUE NOT NULL,
-    nombre      VARCHAR(64)  NOT NULL,
-    apellidos   VARCHAR(64)  NOT NULL,
-    contrasena  VARCHAR(64)  NOT NULL,
-    telefono    VARCHAR(16)  UNIQUE NOT NULL,
-    fecha_alta  DATE         NOT NULL
-);
-
 
 CREATE TABLE Pelicula (
-    id_pelicula     BIGSERIAL       CONSTRAINT PeliculaPK PRIMARY KEY,
-    titulo          VARCHAR(128)    NOT NULL,
+    id              BIGSERIAL       CONSTRAINT PeliculaPK PRIMARY KEY,
+    titulo          VARCHAR(128)    UNIQUE NOT NULL,
+    descripcion     VARCHAR(1024)   NOT NULL,
     director        VARCHAR(128)    NOT NULL,
-    descripcion     VARCHAR(1024)   NOT NULL, 
     duracion        INT             NOT NULL, 
     precio          FLOAT           NOT NULL,
-    tipo_categoria  VARCHAR(16),
-    CONSTRAINT PeliculaCategoria       FOREIGN KEY (tipo_categoria) REFERENCES Categoria (tipo) ON DELETE SET NULL,
-    CONSTRAINT PrecioPeliculaPositivo  CHECK (precio > 0)
+    id_categoria    BIGINT,
+    id_descuento    BIGINT,
+    CONSTRAINT PeliculaCategoriaFK  FOREIGN KEY (id_categoria) REFERENCES Categoria (id) ON DELETE SET NULL,
+    CONSTRAINT PeliculaDescuentoFK  FOREIGN KEY (id_descuento) REFERENCES Descuento (id) ON DELETE SET NULL,
+    CONSTRAINT DuracionPelicula     CHECK (duracion >= 0),
+    CONSTRAINT PrecioPelicula       CHECK (precio >= 0)
 );
- 
+CREATE INDEX PeliculaCategoria ON Pelicula (titulo);
+CREATE INDEX PeliculaCategoria ON Pelicula (id_categoria);
+
+CREATE TABLE Categoria (
+    id          BIGSERIAL       CONSTRAINT CategoriaPK PRIMARY KEY,
+    nombre      VARCHAR(64)     UNIQUE NOT NULL,
+    descripcion VARCHAR(1024)
+);
+
 CREATE TABLE Alquiler (
-    id_pelicula     VARCHAR(128) NOT NULL,
-    email_cliente   VARCHAR(128) NOT NULL,
+    id              BIGSERIAL   CONSTRAINT AlquilerPK PRIMARY KEY,
+    id_usuario      BIGINT,
+    id_pelicula     BIGINT,
     fecha_venta     DATE         NOT NULL,
     fecha_limite    DATE         NOT NULL,
-    precio_venta    FLOAT        NOT NULL,
-    compartible     BOOLEAN      NOT NULL,   
-    CONSTRAINT AlquilerPK               PRIMARY KEY (id_pelicula,email_cliente,fecha_venta)
-    CONSTRAINT AlquilerPelicula         FOREIGN KEY (id_pelicula)   REFERENCES Pelicula (id_pelicula) ON DELETE CASCADE,
-    CONSTRAINT AlquilerCliente          FOREIGN KEY (email_cliente) REFERENCES Cliente (email)        ON DELETE CASCADE,
-    CONSTRAINT PrecioAlquilerPositivo   CHECK (precio_venta > 0)
-    CONSTRAINT FechaLimiteCorrecta      CHECK (fecha_limite > fecha_venta)
+    importe         FLOAT,
+    compartida      BOOLEAN      NOT NULL,
+    CONSTRAINT AlquilerUsuarioFK    FOREIGN KEY (id_usuario) REFERENCES Usuario (id) ON DELETE SET NULL,
+    CONSTRAINT AlquilerPeliculaFK   FOREIGN KEY (id_pelicula)   REFERENCES Pelicula (id) ON DELETE SET NULL,
+    CONSTRAINT ImporteAlquiler      CHECK (precio_venta >= 0),
+    CONSTRAINT FechaLimiteAlquiler  CHECK (fecha_limite > fecha_venta)
 );
+CREATE INDEX AlquilerUsuario ON Alquiler (id_usuario);
+
+CREATE TABLE Descuento (
+    id              BIGSERIAL       CONSTRAINT DescuentoPK PRIMARY KEY,
+    descripcion     VARCHAR(1024),
+    descuento       INT             NOT NULL,
+    precio_original FLOAT           NOT NULL,
+    CONSTRAINT DescuentoPositivo CHECK (descuento > 0),
+    CONSTRAINT PrecioOriginal CHECK (precio_original > 0)
+);
+
+
+
+/* INSERCION DE DATOS */
+
+INSERT INTO Categoria(nombre,descripcion) VALUES ("sci-fi","ciencia ficcion");
+INSERT INTO Categoria(nombre) VALUES ("comedia");
+
+INSERT INTO Pelicula(titulo, descripcion, director, duracion, precio, id_categoria) VALUES ("peli1", "descripcion1", "director1", 120, 3.99, 0);
+INSERT INTO Pelicula(titulo, descripcion, director, duracion, precio, id_categoria) VALUES ("peli2", "descripcion2", "director2", 120, 3.99, 1);
  
--- version_2/src/main.py
+++ version_2/src/main.py
@@ -29,16 +29,13 @@ def disconnect_db(conn):
 
 ##  INSERT-----------------------------------------------------
 
-# Cliente
-def registrarCliente(conn):
+def registrarUsuario(conn):
     """
-    Crea un nuevo cliente con los datos proporcionados.
+    Crea un nuevo usuario con los datos proporcionados.
     :param conn: conexión abierta a la bd
     :return: Nada
     """
 
-    dni = input('DNI: ')
-    if dni=="": dni=None
     nombre = input('Nombre: ')
     if nombre=="": nombre=None
     apellidos = input('Apellidos: ')
@@ -77,6 +74,14 @@ def registrarCliente(conn):
                 print(f"Error {e.pgcode}: {e.pgerror}")
             conn.rollback()
 
+def alquilar(conn, id_user):
+    """
+    1. Pide el nombre de la película
+    2. Comprueba que no la tenga alquilada
+    3. Confirmar importe
+    4. Crea una nueva fila de alquiler
+    """
+    return 1;
 
 ## SELECT-------------------------------------------------------
 
@@ -111,66 +116,134 @@ def logIn(conn):
             else:
                 print(f"Erro {e.pgcode}: {e.pgerror}")
 
+def buscarPorNombre(conn):
+    """
+    1. Pide el nombre de la película
+    2. La recupera y muestra título, descripción, duración y precio
+    """
+    return 1;
+
+def buscarPorCategoria(conn):
+    """
+    1. Pide el nombre de la categoría
+    2. Busca el id de la categoría con ese nombre
+    3. Recupera todas las pelis con ese id_categoria y muestra su título y precio
+    """
+    return 1;
 
 ## UPDATE-------------------------------------------------------
 
+def cambiarContraseña(conn, id_user):
+    """
+    1. Pide la contraseña antigua
+    2. Pide la contraseña nueva
+    3. Actualiza la contraseña
+    """
+    return 1;
+
+def hacerDescuento(conn, id_user):
+    """
+    1. Busca la película
+    2. Comprueba que el precio de la peli es estrictamente mayor que 0
+    3. Dice el descuento que quiere hacer: 40% p.e.
+    4. Crea una fila en descuento INSERT INTO Descuento VALUES (descripcion,descuento,precio_orig,fecha_inicio,fecha_fin)
+    5. Actualiza en Pelicula el precio al descuento% del descuento
+    """
+    return 1;
+
 ## DELETE-------------------------------------------------------
 
+def borrarUsuario(conn, id_user):
+    """
+    1. Pide la contraseña para confirmar el borrado
+    2. Borra el Usuario y lo manda al menú simple
+    """
+    return 1;
+
+def borrarDescuento(conn, id_user):
+    """
+    1. Pide el título de una película
+    2. Busca si tiene descuento y lo coge
+    3. Actualiza el precio de la película al precio_original de descuento
+    4. Borra el descuento
+    """
+    return 1;
+
+## MULTIPLES DML---------------------------------------------------
+
+def compartirPelicula(conn, id_user):
+    """
+    1. Muestra los id y títulos de las películas alquiladas
+    2. Se lo puedes mandar a un usuario especificando su correo
+    3. Comprueba que el correo exista
+    4. Comprueba que no tenga un alquiler con ese correo y fecha límite posterior a hoy
+    5. Hace un update de compartida a true
+    6. Inserta una nueva fila de alquiler con el id_usuario del email especificado
+    """
+    return 1;
 
 
 ## MENU--------------------------------------------------------
-# admin menu
-def adminMenu(conn, id_cliente):
-    """
-    Imprime un menú de opciones para un usuario logeado, solicita a opción e executa a función asociada.
-    'q' para cerrar sesión y volver al menú base.
-    """
-    MENU_TEXT = """
-      -- MENÚ --
-1- Añadir pelicula           2- Borrar categoria
-3- Crear Categoria           6- Aplicar descuento
-q- Cerrar sesion
-"""
-    while True:
-        print(MENU_TEXT)
-        tecla = input('Opción> ')
-        if tecla == 'q':
-            break
         
 # client menu
-def clientMenu(conn, id_cliente):
+def userMenu(conn, id_user):
     """
     Imprime un menú de opciones para un usuario logeado, solicita a opción e executa a función asociada.
     'q' para cerrar sesión y volver al menú base.
     """
     MENU_TEXT = """
       -- MENÚ --
-1- Cambiar contraseña           2- Buscar peliculas por categoria 
-3- Alquilar una pelicula        4- Borrar cuenta                      
-5- Compartir pelicula           
+1- Cambiar contraseña               2- Buscar pelicula por nombre 
+3- Buscar peliculas por categoria   4- Alquilar una pelicula
+5- Borrar cuenta                    6- Compartir pelicula
+7- Hacer descuento                  8- Borrar descuento             
 q- Cerrar sesion
 """
     while True:
         print(MENU_TEXT)
         tecla = input('Opción> ')
+        if tecla == '1':
+            cambiarContraseña(conn, id_user)
+        if tecla == '2':
+            buscarPorNombre(conn)
+        if tecla == '3':
+            buscarPorCategoria(conn)
+        if tecla == '4':
+            alquilar(conn, id_user)
+        if tecla == '5':
+            borrarUsuario(conn, id_user)
+        if tecla == '6':
+            compartirPelicula(conn, id_user)
+        if tecla == '7':
+            hacerDescuento(conn, id_user)
+        if tecla == '8':
+            borrarDescuento(conn, id_user)
         if tecla == 'q':
             break
         
-# main menu
-def menu(conn):
+# simple menu
+def simpleMenu(conn):
     """
     Imprime un menú de opciones, solicita la opción y executa la función asociada.
     'q' para salir.
     """
     MENU_TEXT = """
       -- MENÚ --
-1- Registrarse                     2- Log in (para mas opciones)
-3- Buscar peliculas por categoria  q- Salir   
+1- Registrarse                  2- Log in (para más opciones)
+3- Buscar pelicula por nombre   4- Buscar peliculas por categoria  
+q- Salir   
 """
-
     while True:
         print(MENU_TEXT)
         tecla = input('Opción> ')
+        if tecla == '1':
+            registrarUsuario(conn)
+        if tecla == '2':
+            logIn(conn)
+        if tecla == '3':
+            buscarPorNombre(conn)
+        if tecla == '4':
+            buscarPorCategoria(conn)
         if tecla == 'q':
             break
         
@@ -185,7 +258,7 @@ def main():
     conn = connect_db()
     print('Conectado.')
     
-    menu(conn)
+    simpleMenu(conn)
 
     disconnect_db(conn)
 
